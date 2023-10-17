2aacc4137f590ee16d3f63d86ccd2fc7cbdd9163
sharami_rotem <sharami.rotem@gmail.com>
2023-10-17 18:43:51 +0300

    data

--- src/components/FullActivity/FullActivity.js
+++ src/components/FullActivity/FullActivity.js
@@ -9,13 +9,14 @@ import { motion, AnimatePresence } from "framer-motion";
 import DivCarusel from '../DivCarusel/DivCarusel';
 
 const FullActivity = (obj) => {
-	//const [data, setData] = useState(getData());
+	const [item, setItem] = useState(obj.data.data.activities.filter(item=>item.id == obj.fullActivityId)[0]);
 
 	const windowSize = useRef([window.innerWidth, window.innerHeight]);
 	
 	//console.log(obj.data.data.activities);
 
-    let item = obj.data.data.activities.filter(item=>item.id == obj.fullActivityId)[0];
+    //let item = ;
+
 	let activities = [];
 	Object.keys(obj.timeLine).forEach(dayKey => {
 		Object.keys(obj.timeLine[dayKey]).forEach(typeKey => {
@@ -62,6 +63,9 @@ const FullActivity = (obj) => {
 		let index = k+1 >= (sorted.length) ? sorted[0].id : sorted[k+1].id;
 		obj.setFullActivityId(index);
 		console.log(k+1);
+
+		setItem(obj.data.data.activities.filter(item=>item.id == index)[0]);
+
 	  }, [obj.setFullActivityId, obj.fullActivityId]);
 
 	  const prevActivity = useCallback(() => {
@@ -74,98 +78,73 @@ const FullActivity = (obj) => {
 		});
 		let index = k-1 == -1 ? sorted[(sorted.length-1)].id : sorted[k-1].id;
 		obj.setFullActivityId(index);
+		setItem(obj.data.data.activities.filter(item=>item.id == index)[0]);
 	  }, [obj.setFullActivityId, obj.fullActivityId]);
 
 
 	
 	return(
-		<div
-		
-			className="full_activity"
-		
-		
-		>
+		<div className="full_activity">
 			<motion.div
-
-
-
 				initial={{ scale: 0.0 }}
 				animate={{scale: 1}}
 				exit={{ scale: 0.0 }}
 				transition={{ duration: 0.2 }}
-
 				style={{ height: windowSize.current[1] + 'px' }}
-				
 				>
 				<div className="full_activity_container">
 					<div
 						className='full_activity_box'
 						style={{ height: (windowSize.current[1]-40) + 'px' }}
 						>
-
-						{/* <DivCarusel items={items}/> */}
-
 						<button className="preview" onClick={()=>prevActivity()}>
 							<div className="preview_next_inner"><span className="button_icon"><CaretLeftFill/></span></div>
 						</button>
-
+						
 						<AnimatePresence 
             				mode='wait'
             			>
-			
-
-
-						<motion.div
-							className="activity_info_box row"
-							key={obj.id}
-							initial={{ x: "-"+windowSize.current[1] + 'px' }}
-							animate={{ x: "0px" }}
-							exit={{ x: ((windowSize.current[1])*2) + 'px' }}
-							transition={{ duration: 0.5 }}
-							
-							
-							>
-							<div className='image_box col-4'>
-								<div
-									className='image_background'
-									style={{background:`url(${item.image}) no-repeat center center`}}
+							<motion.div
+								className="activity_info_box row"
+								key={item.id}
+								initial={{ x: "-"+windowSize.current[1] + 'px' }}
+								animate={{ x: "0px" }}
+								exit={{ x: ((windowSize.current[1])*2) + 'px' }}
+								transition={{ duration: 0.5 }}
 								>
+									<div className='image_box col-4'>
+										<div
+											className='image_background'
+											style={{background:`url(${item.image}) no-repeat center center`}}
+										>
+										</div>
+									</div>
+									<div className='info_box col-8'>
+										<div className='title'>
+											{item.title}
+										</div>
+
+										<div className='time'>
+											<span className='icon'><CalendarEventFill/></span> {moment(item.day).format("D/M")} <span className='icon'><ClockFill/></span> {moment(item.start, "YYYY-MM-dd HH:mm").format("HH:mm")}-{moment(item.end, "YYYY-MM-dd HH:mm").format("HH:mm")}
+										</div>
+										<div className='location'>
+											<span className='icon'><GeoAltFill/></span> <span className='location_text'>{obj.data.data.locations[item.location].title}</span>
+										</div>			
+
+										<div className='description'>
+											{item.description}
+										</div>
+
+										<div className='type_box'>
+											<span className='icon'><MusicNoteBeamed/></span> <span className='type_name'>{obj.data.data.activity_type[item.type].title}</span>
+										</div>
+									</div>
+
+								<div className='one_button'>
+									<button onClick={()=>_zoomInToActivity()}><span className='icon'><XCircleFill/></span></button>
 								</div>
-							</div>
-							<div className='info_box col-8'>
-								<div className='title'>
-									{item.title}
-								</div>
-
-								<div className='time'>
-									<span className='icon'><CalendarEventFill/></span> {moment(item.day).format("D/M")} <span className='icon'><ClockFill/></span> {moment(item.start, "YYYY-MM-dd HH:mm").format("HH:mm")}-{moment(item.end, "YYYY-MM-dd HH:mm").format("HH:mm")}
-								</div>
-								<div className='location'>
-									<span className='icon'><GeoAltFill/></span> <span className='location_text'>{obj.data.data.locations[item.location].title}</span>
-								</div>			
-
-								<div className='description'>
-									{item.description}
-								</div>
-
-								<div className='type_box'>
-									<span className='icon'><MusicNoteBeamed/></span> <span className='type_name'>{obj.data.data.activity_type[item.type].title}</span>
-								</div>
-							</div>
-
-							<div className='one_button'>
-								<button onClick={()=>_zoomInToActivity()}><span className='icon'><XCircleFill/></span></button>
-							</div>
-
-
-						</motion.div>
-
-
-
-
+							</motion.div>
 						</AnimatePresence>
-
-
 						<button className="next" onClick={()=>nextActivity()}>
 							<div className="preview_next_inner"><span className="button_icon"><CaretRightFill/></span></div>
 						</button>
