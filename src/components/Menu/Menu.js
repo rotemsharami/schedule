ae2a8b3e8dd2d99353f67ffa926e73ce79d299e7
Rotem Sharami haLevi <rotemsh@ims.local>
2023-10-23 16:55:06 +0300

    data

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,17 +2,53 @@ import React, { useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import './Menu.scss';
 
+import { ArrowDown, MusicNoteBeamed, GeoAltFill, Activity, ClockFill, CalendarEventFill, XCircleFill, CaretLeftFill, CaretRightFill, CursorFill} from "react-bootstrap-icons";
+
+
 const Menu = (item) => {
 
 	const windowSize = useRef([window.innerWidth, window.innerHeight]);
+
+	
+
+
+
+	console.log(windowSize);
 
 	return(
 		<div
 			className="Menu"
 			style={{ height: (windowSize.current[1]) + 'px' }}
-			
-			>
-			Mnue
+		>
+			<div className='items'>
+				<button className='item'>
+					<div className='icon'><span className="the_icon"><GeoAltFill/></span></div>
+					<div className='text'>Schedule</div>
+				</button>
+			</div>
+			<div className='items'>
+				<button className='item'>
+					<div className='icon'><span className="the_icon"><GeoAltFill/></span></div>
+					<div className='text'>News Flash</div>
+				</button>
+			</div>
+			<div className='items'>
+				<button className='item'>
+					<div className='icon'><span className="the_icon"><GeoAltFill/></span></div>
+					<div className='text'>Hotel Information</div>
+				</button>
+			</div>
+
+			<div className='items'>
+				<button className='item'>
+					<div className='icon'><span className="the_icon"><GeoAltFill/></span></div>
+					<div className='text'>Shoping</div>
+				</button>
+			</div>
+
+
+
+
 		</div>
 	);
 };
